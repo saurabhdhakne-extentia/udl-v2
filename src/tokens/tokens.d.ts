f72c6db6d696b2e3e920fc8e22f8d1e8b7151863
Saurabh Dhakne <saurabh.dhakne@merkle.com>
2026-03-10 18:25:09 +0530

    new token updated

---
+++ src/tokens/tokens.d.ts
@@ -0,0 +1,44 @@
+/**
+ * Do not edit directly, this file was auto-generated.
+ */
+
+export const PrimitiveMode1ColoursPrimaryWhite: string;
+export const PrimitiveMode1ColoursPrimaryBlack: string;
+export const PrimitiveMode1ColoursPrimarySage: string;
+export const PrimitiveMode1ColoursNeutral100: string;
+export const PrimitiveMode1ColoursNeutral200: string;
+export const PrimitiveMode1ColoursNeutral300: string;
+export const PrimitiveMode1ColoursNeutral400: string;
+export const PrimitiveMode1ColoursNeutral500: string;
+export const PrimitiveMode1ColoursNeutral600: string;
+export const PrimitiveMode1ColoursNeutral700: string;
+export const PrimitiveMode1ColoursNeutral800: string;
+export const PrimitiveMode1ColoursNeutral900: string;
+export const PrimitiveMode1ColoursNeutral1000: string;
+export const PrimitiveMode1SpacingSpacing0: number;
+export const PrimitiveMode1SpacingSpacing2: number;
+export const PrimitiveMode1SpacingSpacing4: number;
+export const PrimitiveMode1SpacingSpacing8: number;
+export const PrimitiveMode1SpacingNumber12: number;
+export const PrimitiveMode1SpacingSpacing16: number;
+export const PrimitiveMode1SpacingSpacing20: number;
+export const PrimitiveMode1SpacingSpacing24: number;
+export const PrimitiveMode1SpacingSpacing28: number;
+export const PrimitiveMode1SpacingSpacing32: number;
+export const PrimitiveMode1SpacingSpacing36: number;
+export const PrimitiveMode1SpacingSpacing40: number;
+export const PrimitiveMode1SpacingSpacing44: number;
+export const PrimitiveMode1SpacingSpacing48: number;
+export const PrimitiveMode1SpacingSpacing52: number;
+export const PrimitiveMode1SpacingSpacing56: number;
+export const PrimitiveMode1SpacingSpacing60: number;
+export const PrimitiveMode1SpacingSpacing64: number;
+export const PrimitiveMode1SpacingSpacing68: number;
+export const PrimitiveMode1SpacingSpacing72: number;
+export const PrimitiveMode1SpacingSpacing76: number;
+export const PrimitiveMode1SpacingSpacing80: number;
+export const PrimitiveMode1SpacingSpacing96: number;
+export const PrimitiveMode1SpacingSpacing160: number;
+export const SemanticMode1BackgroundCard: string;
+export const SemanticMode1CtaPrimaryBase: string;
+export const SemanticMode1CtaPrimaryText: string;
